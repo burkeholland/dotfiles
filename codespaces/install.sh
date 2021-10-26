d3c051120ca69a931100ad7126ad539a12129c00
Burke Holland <burkeholland@gmail.com>
2021-10-26 15:16:45 -0500

    Add some logging statements

--- codespaces/install.sh
+++ codespaces/install.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-# Install the auto-suggestions plugin
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# Install the auto-suggestions plugin 
 
 zshrc() {
     echo "==========================================================="
-    echo "                  Import zshrc                   "
+    echo "             cloning zsh-autosuggestions                   "
+    echo "-----------------------------------------------------------"                    
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    echo "==========================================================="
+    echo "                  Import zshrc                             "
     echo "-----------------------------------------------------------"
-    cd $HOME/dotfiles
     cat /workspaces/.codespaces/.persistedshare/dotfiles/codespaces/.zshrc > $HOME/.zshrc
 }
 
