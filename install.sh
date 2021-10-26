95de4bc6473d3529566eff79e6669b572af6fd60
Burke Holland <burkeholland@gmail.com>
2021-10-26 14:14:28 -0500

    bash to sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Install the auto-suggestions plugin
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
