893dda7dbc6ced4b1210f6812874ad240ac35634
Burke Holland <burkeholland@gmail.com>
2021-10-26 14:07:29 -0500

    make file executable?

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Install the auto-suggestions plugin
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
