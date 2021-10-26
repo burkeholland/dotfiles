5b80db8357a2b79c2d1e91936734a9c0638a9b8f
Burke Holland <burkeholland@gmail.com>
2021-10-26 13:52:29 -0500

    add changes based on sukka

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
-#!bin/bash
+#!/bin/bash
 
 # Install the auto-suggestions plugin
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+zshrc() {
+    echo "==========================================================="
+    echo "                  Import zshrc                   "
+    echo "-----------------------------------------------------------"
+    cd $HOME/dotfiles
+    cat ./codespaces/zshrc > $HOME/.zshrc
+}
+
+zshrc
