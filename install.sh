7f193f0200953518025bf18f7cac981fb9d66e7c
Burke Holland <burkeholland@gmail.com>
2021-10-27 14:49:40 -0500

    change path of .zshrc

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ zshrc() {
     echo "==========================================================="
     echo "                  Import zshrc                             "
     echo "-----------------------------------------------------------"
-    cat /workspaces/.codespaces/.persistedshare/dotfiles/.zshrc > $HOME/.zshrc
+    cat .zshrc > $HOME/.zshrc
 }
 
 zshrc
