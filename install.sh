845011758f842b5b545d01d153da53175b939582
Burke Holland <burkeholland@gmail.com>
2021-10-29 11:30:29 -0500

    Add zsh nvm

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ zshrc() {
     echo "-----------------------------------------------------------"                    
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     echo "==========================================================="
+    echo "                 cloning zsh-zsh-nvm                       "
+    echo "-----------------------------------------------------------"                 
+    git clone https://github.com/lukechilds/zsh-nvm ${ZSH_CUSTOM:~/.oh-my-zsh/custom}/plugins/zsh-nvm
+    echo "==========================================================="
     echo "                  Import zshrc                             "
     echo "-----------------------------------------------------------"
     cat .zshrc > $HOME/.zshrc
